a134a7928643fd848c7235b97d33c6e419f38ec7
Ozan Sozuoz <ozansozuoz@hotmail.com>
2020-04-01 23:51:13 +0300

    add background image

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,12 @@
-/* eslint-disable no-extra-parens */
-/* eslint no-extra-parens: "error" */
-
 const container = document.querySelector("#container");
-const columnSize = 16;
-const rowSize = 16;
-const sum = columnSize * rowSize;
-container.style.gridTemplateColumns = `repeat(${columnSize},1fr)`;
-container.style.gridTemplateRows = `repeat(${rowSize},1fr)`;
+const btnReset = document.querySelector("#btn-reset");
+let userInput = 16;
+let gridSize = userInput ** 2;
 
 function createElements() {
-  for (let i = 0; i < sum; i++) {
+  container.style.gridTemplateColumns = `repeat(${userInput},1fr)`;
+  container.style.gridTemplateRows = `repeat(${userInput},1fr)`;
+  for (let i = 0; i < gridSize; i++) {
     const gridElements = document.createElement("div");
     gridElements.classList.add("blocks");
     container.appendChild(gridElements);
@@ -17,6 +14,8 @@ function createElements() {
 }
 
 createElements();
+let blocks = document.querySelectorAll(".blocks");
+addEventListener();
 
 function randomColor() {
   return `hsl(${Math.floor(Math.random() * 360)},${Math.floor(
@@ -25,14 +24,13 @@ function randomColor() {
 }
 
 function darkerShade(rgb) {
-  const sep = rgb.indexOf(",") > -1 ? "," : " ";
-  rgb = rgb.substr(4).split(")")[0].split(sep);
-  console.log(rgb);
+  rgb = rgb.substring(4).split(")")[0].split(","); // make an array of each color
 
   let r = rgb[0];
   let g = rgb[1];
   let b = rgb[2];
 
+  // subtract values by 20% to get a darker shade
   if (r >= 0) {
     r -= r * 0.2;
   }
@@ -46,17 +44,28 @@ function darkerShade(rgb) {
   return `rgb(${r},${g},${b})`;
 }
 
-const blocks = document.querySelectorAll(".blocks");
-blocks.forEach(block => {
-  block.addEventListener("mouseover", e => {
-    if (!e.target.classList.contains(".colored")) {
-      e.target.style.backgroundColor = randomColor();
-    } else {
-      e.target.style.backgroundColor = darkerShade(
-        e.target.style.backgroundColor
-      );
-      console.log(darkerShade(e.target.style.backgroundColor));
-    }
-    e.target.classList.add(".colored");
+function addEventListener() {
+  blocks.forEach(block => {
+    block.addEventListener("mouseover", e => {
+      // Only color blocks that have not been colored
+      if (!e.target.classList.contains(".colored")) {
+        e.target.style.backgroundColor = randomColor();
+      } else {
+        e.target.style.backgroundColor = darkerShade(
+          e.target.style.backgroundColor
+        );
+      }
+      // Tag block as colored
+      e.target.classList.add(".colored");
+    });
   });
+}
+
+btnReset.addEventListener("click", () => {
+  userInput = prompt("Enter size. Ex:(4)=4x4 container");
+  gridSize = userInput ** 2;
+  container.innerHTML = "";
+  createElements();
+  blocks = document.querySelectorAll(".blocks");
+  addEventListener();
 });
